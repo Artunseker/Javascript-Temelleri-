43c8283ca6edc26ef7e715e4f8c7d58cd00dfdd0
ArtunSeker <artunseker2005@gmail.com>
2025-03-17 16:02:11 +0300

    Genel Veri Tipleri

---
+++ main.js
@@ -0,0 +1,27 @@
+console.log('Hello World');
+console.error("This is an error");
+console.warn("This is a warning");
+//var ,let, const
+let age =30;
+age = 31;
+console.log(age);
+//const sabittir.
+
+//const score; // hata alıcaz consta yeniden değer atıyamayız list dizi değiştirebiliriz
+score = 95;
+console.log(score);
+
+//String ,Numbers, Boolean, undefined
+const name='Artun';
+const yaş =30;
+const rating=4.5;
+const isMarried = false;
+const x=null; //gerçekten bir nesne değil sahte return
+const y=undefined;
+let z; //değersiz
+console.log(typeof rating);
+console.log(typeof name);
+console.log(typeof yaş);
+console.log(typeof isMarried);
+console.log(typeof x);
+console.log(typeof z);
