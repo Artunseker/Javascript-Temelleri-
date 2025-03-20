f30080c7abc3adf3cc21b21948315263c72a4fc9
ArtunSeker <artunseker2005@gmail.com>
2025-03-21 00:13:02 +0300

    farklı metodlar ve eylemler

--- Dom.js
+++ Dom.js
@@ -18,3 +18,20 @@ const items = document.querySelectorAll('.item');
 items.forEach(function(item){
     console.log(item);
 })
+
+const ul = document.querySelector('.items');
+//ul.remove();
+//ul.lastElementChild.remove();
+ul.firstElementChild.textContent = 'Ayarlar';
+ul.children[1].innerText = 'Oyun Ayarları'; //farklı bir şekli text in
+ul.lastElementChild.innerHTML='<h4>Çıkış</h4>';//HTML de ekleyebiliriz
+
+const btn =document.querySelector('.btn');
+btn.style.background='red';
+
+btn.addEventListener('click',(e) =>{ //click yerine mouseover gibi farklı eylemlerde koyulabilir
+    e.preventDefault(); //butonun default eventlerini iptal ediyoruz
+    document.querySelector('#my-form').style.background='#ccc';
+    document.querySelector('body').classList.add('bg-dark');
+    document.querySelector('.items').lastElementChild.innerText = 'Çıkış'
+});
